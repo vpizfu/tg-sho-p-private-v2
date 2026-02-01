4eeea69475fe25643ed52e2e2cb43f5e893e75c6
Roma <roma@Daryas-MacBook-Pro.local>
2026-02-01 15:37:55 +0300

    release-0.5.0-try-to-add-macbook-apple-watch-category

--- js-files/core.js
+++ js-files/core.js
@@ -536,7 +536,7 @@ function getPlainSvgPlaceholder() {
     '<div class="placeholder-wrapper bg-gray-100 w-full h-full flex items-center justify-center">' +
       '<svg class="w-10 h-10 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">' +
         '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"' +
-        ' d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2z"/>' +
+        ' d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>' +
         '</svg>' +
     '</div>'
   );
