c8b0ef39b47a3a16aa04c57f02b4ec6dfee08d16
Roma <roma@Daryas-MacBook-Pro.local>
2026-02-06 21:18:43 +0300

    try to add preload only for modal screen

--- js-files/core.js
+++ js-files/core.js
@@ -384,9 +384,10 @@ function switchTab(tabName) {
           modal.classList.remove('hidden');     // только показать
           const scrollContainer = document.querySelector('#modalContent .flex-1');
           if (scrollContainer) scrollContainer.scrollTop = modalSavedScrollTop;
-          renderShop()
+          // renderShop()
+          restoreTabScroll('shop');
         } else {
-          renderShop()
+          // renderShop()
           restoreTabScroll('cart');
         }
       }      
