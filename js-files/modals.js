5fbe15725e7a4a42c4996934809f48dcd12c76eb
Roma <roma@Daryas-MacBook-Pro.local>
2026-02-08 13:54:23 +0300

    release-3.0.0

--- js-files/modals.js
+++ js-files/modals.js
@@ -527,17 +527,15 @@ function renderProductModal(product) {
             .map(option => {
               const isSelected = selectedOption[type] === option;
               return (
-                '<button class="option-btn px-3 py-1.5 text-xs font-medium rounded-full scroll-item ' +
-                  (isSelected
-                    ? 'bg-blue-500 text-white border-blue-500 shadow-md font-bold'   // выбрана
-                    : '') +                                                          // НЕ добавляем серый сюда
-                '"' +
-                ' data-type="' + type + '"' +
-                ' data-option="' + escapeHtml(option) + '"' +
-                ' onclick="selectOptionNoFocus(\'' + type + '\', \'' + escapeHtml(option) + '\'); return false;">' +
-                  escapeHtml(option) +
+                '<button class="option-btn px-3 py-1.5 text-xs font-medium scroll-item' +
+                  (isSelected ? ' selected' : '') +
+                  '"' +
+                  ' data-type="' + type + '"' +
+                  ' data-option="' + escapeHtml(option) + '"' +
+                  ' onclick="selectOptionNoFocus(\'' + type + '\', \'' + escapeHtml(option) + '\'); return false;">' +
+                    escapeHtml(option) +
                 '</button>'
-              );
+              );              
             })        
             .join('') +
 
