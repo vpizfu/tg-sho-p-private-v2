0277296e5efb36e755c36ddf659afa7dff1cc4a6
Roma <roma@Daryas-MacBook-Pro.local>
2026-02-23 20:47:48 +0300

    new

--- js-files/ui_common.js
+++ js-files/ui_common.js
@@ -73,22 +73,3 @@
       }
     }
   });
-
-  (function patchTelegramAlertFallback() {
-    if (!window.Telegram || !window.Telegram.WebApp) {
-      window.Telegram = {
-        WebApp: {
-          showAlert(message) {
-            window.alert(String(message));
-          }
-        }
-      };
-      return;
-    }
-  
-    if (typeof window.Telegram.WebApp.showAlert !== 'function') {
-      window.Telegram.WebApp.showAlert = function (message) {
-        window.alert(String(message));
-      };
-    }
-  })();  
